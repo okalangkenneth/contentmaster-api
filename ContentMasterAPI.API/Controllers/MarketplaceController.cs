79885e45138325271471612542ec38cc73618830
okalangkenneth <okalang.ok@gmail.com>
2026-06-24 22:35:50 +0200

    chore: remove AfterTargets publish step and hardcoded runtime identifiers from csproj

--- ContentMasterAPI.API/Controllers/MarketplaceController.cs
+++ ContentMasterAPI.API/Controllers/MarketplaceController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ContentMasterAPI.Core.Models;
 using ContentMasterAPI.Core.Interfaces;
-using ContentMasterAPI.Core.Interfaces;
 
 namespace ContentMasterAPI.API.Controllers
 {
